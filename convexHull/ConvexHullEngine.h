f57a469eff5842cdfe3782f2fb76c2101d158a4d
quimperval <joaquinperezvalera@gmail.com>
2024-04-16 19:42:09 -0600

    Added unit testing for convex hull with sample sets of points. Added method for joining vectors.

--- convexHull/ConvexHullEngine.h
+++ convexHull/ConvexHullEngine.h
@@ -22,19 +22,66 @@ class ConvexHullEngine{
             return true;
         }
 
-        std::vector<Point> getConvexHull(std::vector<Point>& inputVector){
+        std::unique_ptr<std::vector<Point>> getConvexHull(std::vector<Point>& inputVector){
             //First get the inputVector Ordered
+            //std::cout << "###### Printing unordered vector\n";
+            ///this->printVector(inputVector);
             this->orderPoints(inputVector);
             
-            auto response = std::vector<Point>();
-
-            for(int i=0; i< inputVector.size(); i++){
-                std::cout <<"i: " << i << ", x:" <<inputVector.at(i).x << ", y: " << inputVector.at(i).y << std::endl;
+            std::unique_ptr<std::vector<Point>> upperHullList = std::make_unique<std::vector<Point>>();
+            //std::cout << "###### Printing ORDERED vector\n";
+            //this->printVector(inputVector);
+            
+            upperHullList->push_back(inputVector.at(0));
+            upperHullList->push_back(inputVector.at(1));
+            //std::cout << "###### Printing printing temp vector\n";
+            //this->printVector(upperHullList);
+            int counter=0;
+            for(int i=2; i < inputVector.size(); i++){
+                //std::cout<< "@@@@@@ Iteration in algorithm: "<< ++counter << std::endl;
+                
+                upperHullList->push_back(inputVector.at(i));
+                //std::cout<< "@@@@@@ Printing current vector" << std::endl;
+                //this->printVector(upperHullList);
+                while(upperHullList->size()>2 && 
+                this->getTurn(
+                    std::make_unique<Point>(upperHullList->at(upperHullList->size()-3)), 
+                    std::make_unique<Point>(upperHullList->at(upperHullList->size()-2)),
+                    std::make_unique<Point>(upperHullList->at(upperHullList->size()-1)))!=-1){
+                    //std::cout<< "Removing element at: " << (upperHullList->size()-2) << std::endl;
+                    upperHullList->erase(upperHullList->begin() + upperHullList->size()-2);
+                }
             }
-            return std::vector<Point>();
+            //std::cout << "###### Printing upper hull\n";
+            //this->printVector(upperHullList);
+            std::vector<Point> lowerHullList;
+
+            lowerHullList.push_back(inputVector.at(inputVector.size()-1));
+            lowerHullList.push_back(inputVector.at(inputVector.size()-2));
+            //std::cout << "###### Calculating the lower hull\n";
+            counter=0;
+            for(int i=inputVector.size()-3; i>=0; i--){
+                //std::cout<< "@@@@@@ Iteration in algorithm: "<< ++counter << std::endl;
+                lowerHullList.push_back(inputVector.at(i));
+                //std::cout<< "@@@@@@ Printing current vector" << std::endl;
+                while(lowerHullList.size()>2 && 
+                this->getTurn(
+                    std::make_unique<Point>(lowerHullList.at(lowerHullList.size()-3)), 
+                    std::make_unique<Point>(lowerHullList.at(lowerHullList.size()-2)),
+                    std::make_unique<Point>(lowerHullList.at(lowerHullList.size()-1)))!=-1 ){
+                        //std::cout<< "Removing element at: " << (lowerHullList.size()-2) << std::endl;
+                        lowerHullList.erase(lowerHullList.begin() + lowerHullList.size()-2);
+                }
+            }
+
+            ///std::cout << "###### Printing lower hull\n";
+            //this->printVector(lowerHullList);
+            mergeVectors(upperHullList, lowerHullList);
+            //this->printVector(upperHullList);
+            return upperHullList;
         }
 
-        int getTurn(std::unique_ptr<Point>& refPoint, std::unique_ptr<Point>& p1, std::unique_ptr<Point>& p2){
+        int getTurn (const std::unique_ptr<Point>& refPoint, const std::unique_ptr<Point>& p1, const std::unique_ptr<Point>& p2){
             //Let generate a vector, in the sense of the geometry, not in the sense of c++
             // u  = u_1* i + u_2*j + u_3*k;
             // v  = v_1* i + v_2*j + v_3*k;
@@ -52,8 +99,10 @@ class ConvexHullEngine{
             auto kComp = u.x*v.y - u.y*v.x;
             //The other two components are zero, because the z component of the inputs is zero.
             if(kComp>0){
+                //Left turn
                 return 1;
             } else if (kComp<0){
+                //Right turn
                 return -1;
             }
             return 0;
@@ -66,5 +115,33 @@ class ConvexHullEngine{
             return p1.x < p2.y;
         }
 
+        void printVector(std::unique_ptr<std::vector<Point>>& mVector){
+            for(int pos=0; pos< mVector->size(); pos++){
+                std::cout <<"pos: " << pos << ", x:" <<mVector->at(pos).x << ", y: " << mVector->at(pos).y << std::endl;                    
+            }
+        }
+
+        //std::vector<Point>& inputVector
+        void printVector(std::vector<Point> mVector){
+            for(int pos=0; pos< mVector.size(); pos++){
+                std::cout <<"pos: " << pos << ", x:" <<mVector.at(pos).x << ", y: " << mVector.at(pos).y << std::endl;                    
+            }
+        }
+
+        void mergeVectors(std::unique_ptr<std::vector<Point>>& upperHullList, std::vector<Point> lowerHullList){
+            //std::cout<<"Merging vectors" << std::endl;   
+            Point* lastPoint = &upperHullList->at(upperHullList->size()-1);
+            //std::cout << "Last upper point, x:" << lastPoint->x << "; y: " << lastPoint->y << std::endl; 
+            Point* firstPoint = &lowerHullList.at(0);
+            //std::cout << "Fist lower point, x:" << firstPoint->x << "; y: " << firstPoint->y << std::endl; 
+            bool areEqual = (lastPoint->x == firstPoint->x) && (lastPoint->y == firstPoint->y) ;
+            //std::cout << "areEqual: " << areEqual << std::endl;
+            if(areEqual){
+                upperHullList->insert(upperHullList->end(), lowerHullList.begin()+1, lowerHullList.end());
+            } else {
+                upperHullList->insert(upperHullList->end(), lowerHullList.begin(), lowerHullList.end());
+            }
+            
+        }
 };
 #endif
--- convexHull/ConvexHullTest.cpp
+++ convexHull/ConvexHullTest.cpp
@@ -34,9 +34,30 @@ TEST_F(ConvexHullTesting, testHavingMinLengthB){
     ASSERT_FALSE(engine->isValidVector(points));
 }
 
-TEST_F(ConvexHullTesting, DISABLED_getConvexHull){
-    std::vector<Point> points = { {1, 3}, {2, 2}, {1, 2}, {3, 1} };
-    ASSERT_TRUE(engine->getConvexHull(points).size()>=3);
+TEST_F(ConvexHullTesting, getConvexHullA){
+    std::vector<Point> points = { {1, 3}, {2, 1}, {2, 2}, {3, 2}, {1,0},{2,0},{1,1} };
+
+    std::vector<Point> expectedPoints = { {1, 0}, {1, 3}, {3, 2}, {2,0},{1,0}};
+    ASSERT_THAT(expectedPoints.size(), engine->getConvexHull(points)->size());
+}
+
+
+TEST_F(ConvexHullTesting, getConvexHullB){
+    std::vector<Point> points = { {41.7500,59.7500},{65.2500,74.7500},{101.5000,57.7500},
+        {48.0000,37.2500},{62.2500,62.5000},{58.7500,32.0000},{82.0000,41.5000},{78.2500,55.0000},
+        {60.7500,45.2500},{41.0000,47.7500},{36.2500,26.7500},{47.2500,20.5000},{57.5000,19.7500},
+        {69.5000,24.5000},{69.2500,35.0000},{76.2500,34.0000},{90.2500,25.5000},{105.2500,30.7500},
+        {98.5000,43.7500},{92.5000,55.0000},{101.0000,73.7500},{122.5000,62.5000},{116.7500,48.7500},
+        {120.2500,35.0000},{114.0000,19.0000},{85.5000,4.5000},{78.7500,14.2500},{80.7500,19.0000},
+        {67.7500,12.5000},{57.2500,5.7500},{50.2500,5.7500},{40.7500,11.0000},{28.5000,18.2500},
+        {24.2500,29.5000},{29.5000,36.7500},{33.7500,40.0000},{29.5000,53.2500},{34.7500,60.7500},
+        {44.2500,71.5000},{62.5000,77.5000},{82.0000,77.0000},{94.2500,57.0000} };
+
+    std::vector<Point> expectedPoints = {{24.2500,29.5000},{29.5000,53.2500},{34.7500,60.7500},
+        {44.2500,71.5000},{62.5000,77.5000},{82.0000,77.0000},{101.0000,73.7500},{122.5000,62.5000},
+        {120.2500,35.0000},{114.0000,19.0000},{85.5000,4.5000},{50.2500,5.7500},{40.7500,11.0000},
+        {28.5000,18.2500},{24.2500,29.5000}};
+    ASSERT_THAT(expectedPoints.size(), engine->getConvexHull(points)->size());
 }
 
 TEST_F(ConvexHullTesting, turnRightTestA){
@@ -60,9 +81,14 @@ TEST_F(ConvexHullTesting, noTurnTestA){
     ASSERT_EQ(0, engine->getTurn(p1, p2, p3));
 }
 
-TEST_F(ConvexHullTesting, testHavingRequiredLength){
-    std::vector<Point> points = { {1, 3}, {2, 2}, {1, 2},};
+TEST_F(ConvexHullTesting, testVectorHavingRequiredLength){
+    std::vector<Point> points = { {1, 3}, {2, 2}, {1, 2}};
     ASSERT_TRUE(engine->isValidVector(points));
+}
+
+TEST_F(ConvexHullTesting, testVectorNotHavingRequiredLength){
+    std::vector<Point> points = { {1, 3}, {2, 2}};
+    ASSERT_FALSE(engine->isValidVector(points));
 }
 
 int main(int argc, char **argv){
