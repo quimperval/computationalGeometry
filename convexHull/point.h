ea0d21a5b57ec22dfc6b9e6b74a77b56f10cbccf
Joaquin <joaquinperezvalera@gmail.com>
2024-12-02 18:41:59 -0600

    Added logical comparator override for point class

--- convexHull/point.h
+++ convexHull/point.h
@@ -30,6 +30,46 @@ class Point{
         }
 };
 
+bool operator==(const Point& e1, const Point& e2){
+    
+    return (e1.getX()==e2.getX()) && (e1.getY() == e2.getY());
+}
+
+bool operator!=(const Point& e1, const Point& e2){
+    
+    return (e1.getX()!=e2.getX()) || (e1.getY() != e2.getY());
+}
+
+bool operator>(const Point& e1, const Point& e2){
+    //std::cerr << "Debug: greater than" << std::endl;
+    if(e1.getY()>e2.getY()){    
+        //std::cout << "Debug: e1.getY() > e2.getY()" << std::endl;
+
+        return true;
+    } else if (e1.getY()==e2.getY()){
+        //std::cout << "Debug: e1.getY() == e2.getY() and e1.getX() > e2.getX()" << std::endl;
+        if(e1.getX()>e2.getX()){
+            return true;
+        } 
+    } 
+
+
+
+    return false;
+}
+
+bool operator<(const Point& e1, const Point& e2){
+std::cerr << "Debug: less than" << std::endl;
+    if(e1.getY()<e2.getY()){
+        return true;
+    } else if (e1.getY()==e2.getY()){
+        if(e1.getX()<e2.getX()){
+            return true;
+        } 
+    } 
+    return false;
+}
+
 std::ostream& operator<<(std::ostream& os, 
 const Point& point) {
     os << "(" << point.x << ", " 
