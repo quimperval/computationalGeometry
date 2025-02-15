d972be44f4c792a8e1adfee11ef17775ed8e2d72
quimperval <joaquinperezvalerq@gmail.com>
2025-02-15 07:38:25 -0600

    partial work on implementing intersection finder

--- LineSegmentIntersections/IntersectionFinder.h
+++ LineSegmentIntersections/IntersectionFinder.h
@@ -84,10 +84,21 @@ class IntersectionFinder{
                 //std::cout << nextPoint->elem();
         	std::cout << "**********" << std::endl;
 		std::cout << "Processing point " << *p << std::endl;
-
+                //TO-DO: print the lines associated to the event point  
 		std::cout << "**********" << std::endl;
-                //handle event point
-                handleEventPoint(p);
+                //Create a temp sweep line basedon the event poin. Take its ycoordinate, and take the max a min x of all the event points.
+		float minX = eventQueue->getMinX();
+		float maxX = eventQueue->getMaxX();
+
+		std::cout << "###### minX: " << minX << std::endl;
+		std::cout << "###### maxX: " << maxX << std::endl;
+
+		Point* p1 = new Point(minX-10.0f, p->getY());
+		Point* p2 = new Point(maxX+10.0f, p->getY());
+		Line* sweepL = new Line(p1,p2);
+		//handle event point
+
+                handleEventPoint(p, sweepL);
                 
                 
             }
@@ -99,7 +110,10 @@ class IntersectionFinder{
         
 
     private:
-        void handleEventPoint(EventPoint* p){
+	/*
+	 * swl: sweepLine
+	 */
+        void handleEventPoint(EventPoint* p, Line* swl){
 
             //Find all segments stored in the status structure
             //that contain p
@@ -146,21 +160,27 @@ class IntersectionFinder{
 
                 //Then report p as an intersection, together with L(p), 
                 //U(p) and C(p)
+		    std::cout << *p << " is an intersection" << std::endl;
 	    }
            for(auto elem : unionLpCp)
 	   {
             //Delete the segments in L(p) union C(p) from 
             //the status structure
+	    statusStr->removeLine(&elem);
 	   }
 
 	   for(auto elem : unionUpCp)
 	   {
             //personal comment: first remove them from the status structure, and then reinsert.
+            std::cout << "@+@+@+@+@+@+@+@+@+ Status Structure size: " << statusStr->size() << std::endl;
+	    statusStr->removeLine(&elem);
 
+            std::cout << "@+@+@+@+@+@+@+@+@+ Status Structure size after removal of element: " << statusStr->size() << std::endl;
             //Insert the seguments that exist in the union of
             //U(p) and C(p) into the status structure, for this is necessary 
             //the sweep line
             //Deleting and re-inserting the segments of C(p) reverses their order
+            statusStr->addLine(&elem,swl);
             }
 
 	   if(unionUpCp.size()==0)
@@ -168,7 +188,18 @@ class IntersectionFinder{
             //If the union of U(p) and C(p) is the empty set
                 //Then let sj and sr be the left and right neighbors of p 
                //in the status structure.
+               auto sj = statusStr->getSuccesor(*p);
+              
                     //Execute the FindNewEvents(sl, sr, p)
+	        std::cout << "unionUpCP size is zero" << std::endl;
+		if(sj!=nullptr){
+		    std::cout << "pointer to sj is not null: " << sj << std::endl;
+                    std::cout << "sj is: " << *sj << std::endl;
+		}
+            }
+	    else
+	    {
+		std::cout << "unionUpCp size is not zero" << std::endl;
                 //Else: Let s' be the leftmost segment of U(p) union C(p) 
                 //in the status structure
                     //Let sl be the left neighbor of s' in the status structure
